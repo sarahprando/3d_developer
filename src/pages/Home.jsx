77333f03d01b52b5d852c037fa693e0063744fdb
Sarah Prando <sarahprando40@gmail.com>
2024-03-11 19:19:25 -0300

    init commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,64 @@
+import { Suspense } from 'react';
+import { Canvas } from '@react-three/fiber';
+import Loader from '../components/Loader';
+import Island from '../components/models/Island';
+import Sky  from '../components/models/Sky';
+import Bird from '../components/models/Bird';
+import Plane from '../components/models/Plane';
+
+{/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        POPUP
+  </div> */}
+
+const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPosition, rotation]
+  }
+
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize();
+
+  return (
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader TGBFV H />}>
+          <directionalLight
+            position={[1, 1, 1]}
+            intensity={2}
+          />
+          <ambientLight
+            intensity={0.5}
+          />
+          <hemisphereLight
+            skyColor="b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+          />
+
+          <Bird />
+          <Sky />
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+          />
+          <Plane />
+        </Suspense>
+      </Canvas>
+    </section>
+  )
+}
+
+export default Home;
