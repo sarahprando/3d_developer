1eb4de9c05fce455de051f7b510bb3d79581c1e8
Sarah Prando <sarahprando40@gmail.com>
2024-03-14 19:01:07 -0300

    remove hero

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,10 +1,8 @@
-import hero from './hero.jpg'
 import paschoalotto from './paschoalotto.png'
 import auaha from './auaha.png'
 import logo from './logo.svg'
 
 export {
-    hero,
     paschoalotto,
     auaha,
     logo
